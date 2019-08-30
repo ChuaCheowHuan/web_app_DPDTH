7deead99e59c81f1a9c51c468f0c02859260ed09
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-08-31 03:32:16 +0800

    test heroku

--- airline/settings.py
+++ airline/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
+import django_heroku
 import dj_database_url
 import os
 
@@ -129,3 +130,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
