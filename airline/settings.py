4016c8ba61d586c8ae6c19adcdaaaccd0e2e0f98
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-08-31 16:09:54 +0800

    test heroku

--- airline/settings.py
+++ airline/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '',
+#        'PASSWORD': '',
 #        'HOST': 'db',
         'HOST': 'testdb',
         'PORT': 5432,
