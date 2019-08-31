ca69bb1a1f056e5a8ead3ebeed6f15682637ad3d
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-09-01 00:04:33 +0800

    test Django + Postgres in Docker with Travis to Heroku

--- airline/settings.py
+++ airline/settings.py
@@ -82,11 +82,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+#        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': os.environ['DATABASE_HOST'],
         'PORT': os.environ['DATABASE_PORT'],
     }
 }
+
 """
 DATABASES = {
     'default': {
