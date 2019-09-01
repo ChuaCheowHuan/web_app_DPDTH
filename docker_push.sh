cf3dfbea842b26f30726360e9f7010089904ffe0
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-09-01 15:32:12 +0800

    test Django + Postgres in Docker with Travis to Heroku

---
+++ docker_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#docker push USER/REPO
+
+
+docker ps
+sudo docker login --username $HEROKU_DOCKER_USERNAME --password $HEROKU_AUTH_TOKEN registry.heroku.com
+sudo docker tag webapp-dpdth:latest registry.heroku.com/webapp-dpdth/web
+sudo docker inspect --format='{{.Id}}' registry.heroku.com/webapp-dpdth/web
+if [ $TRAVIS_BRANCH == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then sudo docker push registry.heroku.com/webapp-dpdth/web; fi
+
+#    - heroku addons:create heroku-postgresql:hobby-dev -a mywebapp0
+
+#    - heroku run container:release web -a webapp-dpdth
+#    - heroku run python manage.py makemigrations -a webapp-dpdth
+#    - heroku run python manage.py migrate -a webapp-dpdth
+./heroku-container-release.sh
