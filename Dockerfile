1a05f3ac23a027d8770b0a552942db5146beb278
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-09-01 18:35:21 +0800

    test Django + Postgres in Docker with Travis to Heroku

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 FROM python:3
+
 WORKDIR /usr/src/app
+
 ADD requirements.txt /usr/src/app
+
 RUN pip install -r requirements.txt
+
+# collect static files
+RUN python manage.py collectstatic --noinput
+
 ADD . /usr/src/app
+
 # run gunicorn
 #CMD gunicorn --bind 0.0.0.0:$PORT wsgi
 #CMD gunicorn hello_django.wsgi:application --bind 0.0.0.0:$PORT
--- airline/settings.py
+++ airline/settings.py
@@ -111,11 +111,11 @@ DATABASES = {
         #'PORT': 5433,
     }
 }
-"""
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
