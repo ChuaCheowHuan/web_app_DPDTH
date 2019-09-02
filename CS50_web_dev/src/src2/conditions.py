d2b0bcb311e01369dcc988c7279b28907643c8fc
Chua Cheow Huan <17569306+ChuaCheowHuan@users.noreply.github.com>
2019-09-02 21:56:25 +0800

    edit .gitignore

--- CS50_web_dev/src/src2/conditions.py
+++
@@ -1,8 +0,0 @@
-x = int(input())
-
-if x > 0:
-    print("x is positive")
-elif x < 0:
-    print("x is negative")
-else:
-    print("x is 0")
--- CS50_web_dev/src/src7/mysite/hello/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world!")
-
-def second(request):
-    return HttpResponse("Second page")
